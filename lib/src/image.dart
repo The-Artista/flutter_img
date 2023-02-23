26bc50140ec47cdba21b2f4b73892b2ea926411c
ishaf <shafiulislam20@gmail.com>
2023-02-23 20:06:20 +0600

    image height and width

--- lib/src/image.dart
+++ lib/src/image.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
@@ -18,6 +21,7 @@ class Img extends StatelessWidget {
   final Color? bgColor;
   final ColorFilter? colorFilter;
   final BoxFit? fit;
+  final Widget? placeholder;
   final String? blurHash;
   final Widget child;
 
@@ -26,6 +30,7 @@ class Img extends StatelessWidget {
     super.key,
     this.height,
     this.blurHash,
+    this.placeholder,
     this.width,
     this.ratio,
     this.shape,
@@ -45,18 +50,27 @@ class Img extends StatelessWidget {
     final svgRegX = RegExp(r'<\s*svg[^>]*>(.*?)<\s*/\s*svg>');
     if (svgRegX.hasMatch(src)) {
       return Container(
-        height: height,
+        height: height ?? MediaQuery.of(context).size.height,
         width: width ?? MediaQuery.of(context).size.width,
         margin: margin,
         padding: padding,
-        decoration: SvgDecoration.string(src), // need to marge with ImageProvider
+        decoration: SvgDecoration.string(src),
+        // need to marge with ImageProvider
         child: child,
       );
-    }
-    if(getImageType(src) == "asset"){
+    } else if (getImageType(src) == "asset") {
+      double? calHeight = 0;
+      double? calWidth = 0;
+      Image image = Image.asset(src);
+      Completer<ui.Image> completer = Completer<ui.Image>();
+      image.image
+          .resolve(const ImageConfiguration())
+          .addListener(ImageStreamListener((ImageInfo info, bool _) {
+        completer.complete(info.image);
+      }));
       return Container(
-        height: height,
-        width: width ?? MediaQuery.of(context).size.width,
+        height: height ?? calHeight,
+        width: width ?? calWidth,
         margin: margin,
         padding: padding,
         decoration: BoxDecoration(
@@ -65,50 +79,119 @@ class Img extends StatelessWidget {
           border: border,
           color: bgColor,
           image: DecorationImage(
-            fit: fit ?? BoxFit.fill,
+            fit: fit,
             colorFilter: colorFilter,
             image: getImageProvider(src),
           ),
         ),
         child: child,
       );
+    } else {
+      final svgExtRegX = RegExp(r'\.(svg)(?:\?.*|)$');
+      if (svgExtRegX.hasMatch(src)) {
+        return Container(
+          height: height,
+          width: width ?? MediaQuery.of(context).size.width,
+          margin: margin,
+          padding: padding,
+          decoration: BoxDecoration(
+            shape: shape ?? BoxShape.rectangle,
+            borderRadius: borderRadius,
+            border: border,
+            color: bgColor,
+            image: DecorationImage(
+              fit: fit,
+              colorFilter: colorFilter,
+              image: getImageProvider(src),
+            ),
+          ),
+          // need to marge with ImageProvider
+          child: child,
+        );
+      } else {
+        double? calHeight = 0;
+        double? calWidth = 0;
+        Image image = Image.network(src);
+        Completer<ui.Image> completer = Completer<ui.Image>();
+        image.image
+            .resolve(const ImageConfiguration())
+            .addListener(ImageStreamListener((ImageInfo info, bool _) {
+          completer.complete(info.image);
+        }));
+        return Expanded(
+          child: FutureBuilder<ui.Image>(
+            future: completer.future,
+            builder:
+                (BuildContext context, AsyncSnapshot<ui.Image> snapshot) {
+              if (snapshot.hasData) {
+                final ratio = MediaQuery.of(context).size.width/snapshot.data!.width;
+                calWidth = ratio * snapshot.data!.width;
+                calHeight = ratio * snapshot.data!.height;
+              }
+              return CachedNetworkImage(
+                imageUrl: src,
+                imageBuilder: (context, imageProvider) {
+                  return Container(
+                    height: height ?? calHeight,
+                    width: width ?? calWidth,
+                    margin: margin,
+                    padding: padding,
+                    decoration: BoxDecoration(
+                      shape: shape ?? BoxShape.rectangle,
+                      borderRadius: borderRadius,
+                      border: border,
+                      color: bgColor,
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: fit,
+                        colorFilter: colorFilter,
+                      ),
+                    ),
+                    // need to marge with ImageProvider
+                    child: child,
+                  );
+                },
+                height: height ?? calHeight,
+                width: width ?? calWidth,
+                placeholder: (context, url) {
+                  return placeholder ??
+                      BlurHash(hash: blurHash ?? "L5H2EC=PM+yV0g-mq.wG9c010J}I");
+                },
+              );
+            },
+          ),
+        );
+      }
     }
-    else{
-     return CachedNetworkImage(
-        imageUrl:
-        src,
-        imageBuilder: (context, imageProvider) {
-          return Container(
-            height: height,
-            width: width ?? MediaQuery.of(context).size.width,
-            margin: margin,
-            padding: padding,
-            decoration: BoxDecoration(
-              shape: shape ?? BoxShape.rectangle,
-              borderRadius: borderRadius,
-              border: border,
-              color: bgColor,
-              image: DecorationImage(
-                image: imageProvider,
-                fit: fit ?? BoxFit.fill,
-                colorFilter: colorFilter,
-              ),
-            ), // need to marge with ImageProvider
-            child: child,
-          );
-        },
-        height: height,
-        width: width ?? MediaQuery.of(context).size.width,
-        placeholder: (context, url) {
-          return  BlurHash(hash: blurHash ?? "L5H2EC=PM+yV0g-mq.wG9c010J}I");
-        },
-      );
-    }
-
   }
 }
 
 ImageProvider<Object> getImageProvider(String src) {
+  final httpRegX = RegExp(
+    r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])',
+  );
+  final svgExtRegX = RegExp(r'\.(svg)(?:\?.*|)$');
+  if (httpRegX.hasMatch(src)) {
+    if (svgExtRegX.hasMatch(src)) {
+      return SvgProvider(
+        src,
+        source: SvgSource.network,
+      );
+    } else {
+      return NetworkImage(src);
+    }
+  } else {
+    if (svgExtRegX.hasMatch(src)) {
+      return SvgProvider(
+        src,
+      );
+    } else {
+      return AssetImage(src);
+    }
+  }
+}
+
+ImageProvider<Object> getImage(String src) {
   final httpRegX = RegExp(
     r'(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])',
   );
